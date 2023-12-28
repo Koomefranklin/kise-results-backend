3815d93b4e7e0d6c4662b2f2c6511ad6127d2214
koome <frankoomef@gmail.com>
2023-12-28 21:12:50 +0300

    created the index view and added the app to urls

--- dev/views.py
+++ dev/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.db.models import Q
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import User
 
 # Create your views here.
+
+# def get_context_data(self, **kwargs):
+#     context = super().get_context_data(**kwargs)
+#     context['has_permission'] = self.request.user
+#     context['units'] = RegisteredUnit.objects.filter(student__user=self.request.user)
+#     context['student_details'] = Student.objects.get(user=self.request.user)
+#     return context
+
+class IndexPage(LoginRequiredMixin, ListView):
+  template_name = 'results/index.html'
+  context_object_name = 'context'
+  model = User
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['has_permission'] = self.request.user
+    context['is_student'] = True if self.request.user.role == 'student' else False
+    context['is_lecturer'] = not context['is_student']
+    return context
--- results/urls.py
+++ results/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('dev.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("accounts/", include('django.contrib.auth.urls')),    
 ]
+
+admin.site.base_template = 'admin/base_site.html'
