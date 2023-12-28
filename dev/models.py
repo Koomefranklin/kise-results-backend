ac49e0cb043dafba4ff2feca88f1da6fe5769cf8
koome <frankoomef@gmail.com>
2023-12-28 21:10:53 +0300

    created more models

--- dev/models.py
+++ dev/models.py
@@ -8,19 +8,19 @@ class User(AbstractUser):
     ('student', 'Student'),
     ('lecturer', 'Lecturer'),
   ]
-  sirname = models.CharField(max_length=50)
+  surname = models.CharField(max_length=50)
   other_names = models.CharField(max_length=100)
   first_name = None
   last_name = None
   role = models.CharField(choices=ROLES, max_length=10, default='student')
-  REQUIRED_FIELDS = ['sirname', 'other_names', ]
+  REQUIRED_FIELDS = ['surname', 'other_names', ]
 
   def __str__(self):
-    return f'{self.sirname} {self.other_names}'
+    return f'{self.surname} {self.other_names}'
   
 class Session(models.Model):
 
-  class Year(models.IntegerChoices):
+  class Period(models.IntegerChoices):
     First = 1
     Second = 2
 
@@ -28,8 +28,11 @@ class Session(models.Model):
     DISTANCE_LEARNING = 'DL'
     FULL_TIME = 'FT'
 
-  year = models.IntegerField(choices=Year.choices)
+  period = models.IntegerField(choices=Period.choices)
   mode = models.CharField(max_length=2, choices=Mode.choices)
+
+  def __str__(self):
+    return f'{self.mode} Year {self.period}'
 
 class Lecturer(models.Model):
   class Role(models.TextChoices):
@@ -39,3 +42,93 @@ class Lecturer(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   role = models.CharField(max_length=3, choices=Role.choices, default=Role.LECTURER)
 
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=['user',], name='unique_lecturer_user'),
+    ]
+
+  def __str__(self):
+    return str(self.user)
+
+class Student(models.Model):
+  registration = models.CharField(max_length=20, primary_key=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  # year = models.DateField()
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=['user',], name='unique_student_user'),
+    ]
+
+  def __str__(self):
+    return f'{self.registration} {self.user.surname}'
+
+class Course(models.Model):
+  code = models.IntegerField(primary_key=True)
+  name = models.CharField(max_length=50)
+  session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.code} {self.name}'
+
+class Paper(models.Model):
+  code = models.CharField(max_length=8, primary_key=True)
+  name = models.CharField(max_length=100)
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.code} {self.name}'
+
+class TeamLeader(models.Model):
+  lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=['lecturer',], name='unique_teamleader'),
+    ]
+
+  def __str__(self):
+    return self.lecturer
+
+class Specialization(models.Model):
+  student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+  class Meta:
+    constraints = [
+      models.UniqueConstraint(fields=['student', 'course'], name='unique_student_course'),
+    ]
+
+  def __str__(self):
+    return str(self.student)
+  
+class Module(models.Model):
+  code = models.CharField(max_length=10, primary_key=True)
+  name = models.CharField(max_length=200)
+  paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return str(f'{self.code} {self.name}')
+
+class CatCombination(models.Model):
+  cat1 = models.ManyToManyField(Module, related_name='cat1')
+  cat2 = models.ManyToManyField(Module, related_name='cat2')
+  paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return str(self.paper)
+
+class LecturerModule(models.Model):
+  lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+  module = models.ForeignKey(Module, on_delete=models.CASCADE)
+  period = models.CharField(max_length=50)
+
+class Result(models.Model):
+  student = models.ForeignKey(Student, on_delete=models.CASCADE)
+  paper = models.ForeignKey(Paper, on_delete=models.CASCADE)
+  cat1 = models.IntegerField()
+  cat2 = models.IntegerField()
+
+  def __str__(self):
+    return str(f'{self.student} {self.paper}')
