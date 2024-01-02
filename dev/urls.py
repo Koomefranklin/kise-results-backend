c2126c80bc727f82982a08f381939adb8842d691
koome <frankoomef@gmail.com>
2024-01-02 11:45:11 +0300

    added urls for created views

--- dev/urls.py
+++ dev/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from os import name
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-  path('', views.IndexPage.as_view(), name='index')
+  path('', views.IndexPage.as_view(), name='index'),
+  path('paperResult/<str:paper>', views.PaperResult.as_view(), name='single_paper'),
+  path('student', views.StudentResult.as_view(), name='student_result'),
+  path('course/<int:pk>', views.CourseResults.as_view(), name='course_results'),
+  re_path(r'^student-autocomplete/$', views.StudentAutocomplete.as_view(), name='student_autocomplete'),
+  path('update/<str:pk>', views.StudentPaperUpdate.as_view(), name='result_update'),
 ]
