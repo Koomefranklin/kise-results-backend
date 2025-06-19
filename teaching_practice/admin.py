40aafc81c67bb27fa4dd7c7e391d665d30b45edf
Koome Franklin <frankoomef@gmail.com>
2025-06-20 01:08:46 +0300

    changed admin assessment display

--- teaching_practice/admin.py
+++ teaching_practice/admin.py
@@ -13,11 +13,11 @@ class StudentAspectAdmin(admin.ModelAdmin):
     return obj.aspect.name
   
 class StudentLetterAdmin(admin.ModelAdmin):
-  list_display = ['student', 'assessor']
+  list_display = ['student', 'assessor', 'total_score']
   search_fields = ['pk','student__full_name', 'assessor__full_name']
   autocomplete_fields = ['student', 'assessor']
   readonly_fields = ['created_at', 'updated_at']
-  list_filter = ['assessment_type', 'is_editable']
+  list_filter = ['assessment_type', 'is_editable', 'to_delete']
 
 class TpStudentAdmin(admin.ModelAdmin):
   list_display = ['full_name', 'index', 'specialization', 'created_by']
